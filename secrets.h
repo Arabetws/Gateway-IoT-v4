ddc91c6c7cdfce94ce82810e833bc21e72def584
Arabetws <fsilva.mikael@gmail.com>
2026-07-13 04:15:04 -0300

    Add secrets.h with Tuya API credentials

---
+++ secrets.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define DEFAULT_TUYA_CLIENT_ID     "578nvtrent95jqg5t5mv"
+#define DEFAULT_TUYA_CLIENT_SECRET "c2a8c978702e4dbeabd7385b4c606547"
+#define DEFAULT_TUYA_HOST          "openapi.tuyaus.com"
