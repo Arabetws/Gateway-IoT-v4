6e41f3c1481b8a956320dfa9506096116f069565
Arabetws <fsilva.mikael@gmail.com>
2026-07-30 11:40:54 -0300

    Add comments to Tuya client ID and secret
    Updated comments for Tuya client ID and secret.

--- secrets.h
+++ secrets.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define DEFAULT_TUYA_CLIENT_ID     "578nvtrent95jqg5t5mv"
-#define DEFAULT_TUYA_CLIENT_SECRET "c2a8c978702e4dbeabd7385b4c606547"
+#define DEFAULT_TUYA_CLIENT_ID     "578nvtrent95jqg5t5mv" //amm4kqwh4dv89ax3tvmm
+#define DEFAULT_TUYA_CLIENT_SECRET "c2a8c978702e4dbeabd7385b4c606547" //af88d6344d2648d9a338cf63400fbe22
 #define DEFAULT_TUYA_HOST          "openapi.tuyaus.com"
